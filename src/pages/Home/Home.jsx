53870b3f395a5806c0a45e97f3a34909fab86f23
rakibulmd <rakib245875@gmail.com>
2024-01-20 13:26:40 +0600

    retry deploy

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,22 +1,22 @@
-
 import { useRef } from "react";
 import Header from "../../Common/Header";
-import Banner from "./Banner";
 import ClientMarquee from "./ClientMarquee";
 import HeroSection from "./HeroSection";
 import Solutions from "./Solutions";
+import Footer from "../../Common/Footer";
 
 const Home = () => {
-    const solutionRef = useRef(null);
-    return (
-        <>
-            <Header solutionScrollRef={solutionRef}></Header>
-            <HeroSection></HeroSection>
-            <ClientMarquee></ClientMarquee>
-            <Solutions solutionScrollRef={solutionRef}></Solutions>
-            <Banner></Banner>
-        </>
-    );
+  const solutionRef = useRef(null);
+  //   console
+  return (
+    <>
+      <Header solutionScrollRef={solutionRef}></Header>
+      <HeroSection></HeroSection>
+      <ClientMarquee></ClientMarquee>
+      <Solutions solutionScrollRef={solutionRef}></Solutions>
+      <Footer></Footer>
+    </>
+  );
 };
 
 export default Home;
